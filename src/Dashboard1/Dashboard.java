9836037388dc40aa819419d039f17f3c31eabaef
winandtu <55026642+winandtu@users.noreply.github.com>
2021-09-28 12:05:49 -0500

    APLICACION ANTIGUA

--- src/Dashboard1/Dashboard.java
+++ src/Dashboard1/Dashboard.java
@@ -9,7 +9,7 @@ package Dashboard1;
  *
  * @author DANILO
  */
-public class Dashboard extends javax.swing.JFrame {
+public class Dashboard extends javax.swing.JFrame { //APLICACIÓN ANTIGUA
 
     /**
      * Creates new form Dashboard
